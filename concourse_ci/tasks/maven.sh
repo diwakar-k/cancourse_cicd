132408a92a3087893afd7f488446601604402b0c
diwakar.k <diwakar.k@quantiphi.com>
2022-11-17 13:21:41 +0530

    branch

--- concourse_ci/tasks/maven.sh
+++ concourse_ci/tasks/maven.sh
@@ -8,8 +8,8 @@
 
 #!/bin/bash
 
-export NEW_VERSIONS=$(cat ../branches/branches)
-export OLD_VERSIONS=$(cat ../branches/removed)
+export NEW_VERSIONS=$(cat repo/branches/branches)
+export OLD_VERSIONS=$(cat repo/branches/removed)
 
 # fly login -t your-concourse -c http://localhost:8080 -u test -p test
 for version in $NEW_VERSIONS; do
